aced7e56c6439de42d38b5da8b9a931bf8d3cf1a
ejaman <leegm042@gmail.com>
2021-11-25 12:41:43 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>hello</h1>;
+  const [videos, setVideos] = useState([]); // 각각의 변수와 업데이트할 수  있는 함수 할당
+
+  useEffect(() => {
+    console.log("useEffect");
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCdKNDLgpxvybCQp_i6LuubN0cnwnCqIxQ",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []); // 한번만 호출
+  // 원하는 변수(예,[videos])의 목록을 전달하면 얘네들이 업데이트 될 때마다 콜백함수 불려짐
+  // 텅텅 빈 배열([])은 한번만 마운트가 되었을 때, 아무것도 전달하지 않으면 컴포넌트에 state나 prop이 업데이트 될 떄 마다 반복적으로 호출됨
+  return <VideoList videos={videos} />;
 }
 
 export default App;
